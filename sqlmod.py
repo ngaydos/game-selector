9caf423000c3574480661715e223c93aae35c612
Nicholas Gaydos <ngaydos@gmail.com>
2018-02-05 15:54:00 -0800

    change to only add new records

--- sqlmod.py
+++ sqlmod.py
@@ -20,9 +20,12 @@ def insert_rating(game, person, rating):
     conn.close()
 
 def insert_ratings(game, people, ratings):
+    conn = psycopg2.connect('dbname = boardgames user = postgres')
     for value in range(len(people)):
-        conn = psycopg2.connect('dbname = boardgames user = postgres')
         cur = conn.cursor()
+        cur.execute(("SELECT COUNT('rating_id') FROM narrow_ratings WHERE game_id = (SELECT id FROM games WHERE name = '{}') AND person_id = (SELECT id FROM people_index WHERE name = '{}')").format(game, people[value]))
+        if cur.fetchall()[0][0]:
+            continue
         cur.execute(("INSERT INTO narrow_ratings (game_id, person_id, rating) VALUES ((SELECT id FROM games WHERE name = '{}'), (SELECT id FROM people_index WHERE name = '{}'), '{}'); ").format(game, people[value], ratings[value]))
         conn.commit()
-        conn.close()
+    conn.close()
